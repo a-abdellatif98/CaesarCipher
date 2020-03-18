4a8dfed74b1324af63238dda536be90b6907307c
gad <mohamed.gad@insidetrack.com>
2020-03-18 23:02:58 +0200

    tweaks

--- caesar.rb
+++ caesar.rb
@@ -1,26 +1,23 @@
- puts "Word please: "
- text = gets.chomp.downcase
- puts "Number please: "
- n = gets.chomp.to_i
- puts "mode pleas: E or D "
- mode = gets.chomp.downcase
+# frozen_string_literal: true
 
-if  mode.eql?("e")
-  def caesar_cipher(text, n)
-   alphabet = ('a'..'z').to_a 
-   key = Hash[alphabet.zip(alphabet.rotate(n))]
-   return text.each_char.inject("") { |newtext, char| newtext + key[char] }
- end
-elsif  mode.eql?("d")
- def decaesar_cipher(text, n)
-  alphabet = ('a'..'z').to_a 
-  key = Hash[alphabet.zip(alphabet.rotate(-n))]
-  text.each_char.inject("") { |newtext, char| newtext + key[char] }
-  end
+puts 'Word please: '
+text = gets.chomp.downcase
+puts 'Number please: '
+n = gets.chomp.to_i
+puts 'mode pleas: E or D '
+mode = gets.chomp.downcase
+
+@alphabet = ('a'..'z').to_a
+
+def caesar_cipher(text, n)
+  key = Hash[@alphabet.zip(@alphabet.rotate(n))]
+  text.each_char.inject('') { |newtext, char| newtext + key[char] }
 end
-if  mode.eql?("e")
-  x  = caesar_cipher(text, n)
-  puts x 
-else x  = decaesar_cipher(text, n)
-  puts x 
+
+def decaesar_cipher(text, n)
+  key = Hash[@alphabet.zip(@alphabet.rotate(-n))]
+  text.each_char.inject('') { |newtext, char| newtext + key[char] }
 end
+
+result = mode.eql?('e') ? caesar_cipher(text, n) : decaesar_cipher(text, n)
+puts result
